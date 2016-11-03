2e733a1ffbf0d84c9dad610288eda5ab53f3651a
tooleks <tooleks@gmail.com>
2016-11-03 14:16:47 +0200

    Cleaning up

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -45,6 +45,23 @@ class RouteServiceProvider extends ServiceProvider
     }
 
     /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::group([
+            'middleware' => 'web',
+            'namespace' => $this->namespace,
+        ], function ($router) {
+            require base_path('routes/web.php');
+        });
+    }
+
+    /**
      * Define the "api" routes for the application.
      *
      * These routes are typically stateless.
