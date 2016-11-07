ec1ca51ecfad492adf16bb8ac518c3dc1f722790
tooleks <tooleks@gmail.com>
2016-11-07 19:21:54 +0200

    [FE]. Added top bar to the layout

--- frontend/src/vendor.ts
+++ frontend/src/vendor.ts
@@ -10,11 +10,13 @@ import 'rxjs';
 import 'ng2-toastr/bundles/ng2-toastr.min.css';
 import 'ng2-toastr/ng2-toastr';
 
-import '../public/theme/js/inspinia.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import '../public/theme/js/jquery.metismenu.js';
 import '../public/theme/js/jquery.slimscroll.js';
+import '../public/theme/js/inspinia.js';
 
-import '../public/theme/css/bootstrap.css';
 import '../public/theme/font-awesome/css/font-awesome.css';
 import '../public/theme/css/style.css';
 import '../public/theme/css/animate.css';
