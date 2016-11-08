09bbb32cafb3b7cc1923a0135378ad2716f1f724
tooleks <tooleks@gmail.com>
2016-11-08 12:55:21 +0200

    [FE]. Added title to the PhotoFormComponent

--- frontend/src/photos/models/photo-model.ts
+++ frontend/src/photos/models/photo-model.ts
@@ -5,12 +5,11 @@ export class PhotoModel {
     id:number;
     description:string;
     relative_url:string;
-    is_draft:boolean;
     created_at:string;
     updated_at:string;
     is_uploaded:boolean;
+    is_published:boolean;
     absolute_url:string;
-    short_description:string;
     tags:TagModel[] = [];
     thumbnails:ThumbnailModel[] = [];
 }
