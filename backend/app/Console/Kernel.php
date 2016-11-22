1f151dc7186db85934d3214b5e215f6dae4acf4e
tooleks <tooleks@gmail.com>
2016-11-22 13:16:49 +0200

    [BE]. Added cleanup photo file system command to scheduler

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -22,13 +22,12 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('cleanup:photo_file_system')->dailyAt('00:00');
     }
 
     /**
