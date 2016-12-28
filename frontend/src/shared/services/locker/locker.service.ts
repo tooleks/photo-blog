3523b18baa17982c4fc554e11d79208e9d26ab1f
tooleks <tooleks@gmail.com>
2016-12-28 20:13:15 +0200

    [FE]. Locker service refactoring

--- frontend/src/shared/services/locker/locker.service.ts
+++ frontend/src/shared/services/locker/locker.service.ts
@@ -2,11 +2,15 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class LockerService {
-    protected locked:boolean = false;
+    private locked:boolean = false;
 
-    lock = () => this.locked = true;
+    lock = ():void => {
+        this.locked = true;
+    };
 
-    unlock = () => this.locked = false;
+    unlock = ():void => {
+        this.locked = false;
+    };
 
-    isLocked = () => this.locked === true;
+    isLocked = ():boolean => this.locked === true;
 }
