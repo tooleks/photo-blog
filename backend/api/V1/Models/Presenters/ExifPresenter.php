3663a70ebf50820a8974b7197183649691029da5
tooleks <tooleks@gmail.com>
2017-02-06 18:15:37 +0200

    [BE]. Fixed exif presenter issues

--- backend/api/V1/Models/Presenters/ExifPresenter.php
+++ backend/api/V1/Models/Presenters/ExifPresenter.php
@@ -20,15 +20,15 @@ class ExifPresenter extends Presenter
         return [
             'manufacturer' => 'data.Make',
             'model' => 'data.Model',
-            'exposure_time' => function () : string {
-                if ($this->getPresenteeAttribute('data.ExposureTime')) {
-                    list($numerator, $denominator) = explode('/', $this->getPresenteeAttribute('data.ExposureTime'));
-                    return '1/' . $denominator / $numerator;
-                } else {
-                    return null;
+            'exposure_time' => function () {
+                $exposureTime = $this->getPresenteeAttribute('data.ExposureTime');
+                if ($exposureTime) {
+                    list($numerator, $denominator) = explode('/', $exposureTime);
+                    $exposureTime = '1/' . $denominator / $numerator;
                 }
+                return $exposureTime ?? null;
             },
-            'aperture' => 'data.COMPUTED.ApertureFNumber',
+            'aperture' => 'data.COMPUTED.ApertureFNumberd',
             'iso' => 'data.ISOSpeedRatings',
             'taken_at' => function () : string {
                 return new Carbon($this->getPresenteeAttribute('data.DateTime'));
