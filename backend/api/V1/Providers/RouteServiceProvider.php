be59cbb6ae5b30f75c1196b9b2518c0ec5c1aeee
tooleks <tooleks@gmail.com>
2017-02-11 22:34:06 +0200

    [BE]. Cleaning up

--- backend/api/V1/Providers/RouteServiceProvider.php
+++ backend/api/V1/Providers/RouteServiceProvider.php
@@ -25,24 +25,6 @@ class RouteServiceProvider extends ServiceProvider
     protected $namespace = 'Api\V1\Http\Controllers';
 
     /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
-
-        Route::bind('user', function (int $id) {
-            return $this->getModelBindingMap()[Route::currentRouteName()]($id);
-        });
-
-        Route::bind('photo', function (int $id) {
-            return $this->getModelBindingMap()[Route::currentRouteName()]($id);
-        });
-    }
-
-    /**
      * Define the routes for the application.
      *
      * @return void
@@ -55,43 +37,5 @@ class RouteServiceProvider extends ServiceProvider
         ], function ($router) {
             require base_path('routes/api.v1.php');
         });
-    }
-
-    /**
-     * Get model binding map.
-     *
-     * @return array
-     */
-    protected function getModelBindingMap()
-    {
-        return [
-            'update_uploaded_photo' => function ($id) {
-                return $this->app->make(UploadedPhotoService::class)->getById($id);
-            },
-            'get_uploaded_photo' => function ($id) {
-                return $this->app->make(UploadedPhotoService::class)->getById($id);
-            },
-            'delete_uploaded_photo' => function ($id) {
-                return $this->app->make(UploadedPhotoService::class)->getById($id);
-            },
-            'update_photo' => function ($id) {
-                return $this->app->make(PhotoService::class)->getById($id);
-            },
-            'get_photo' => function ($id) {
-                return $this->app->make(PhotoService::class)->getById($id);
-            },
-            'delete_photo' => function ($id) {
-                return $this->app->make(PhotoService::class)->getById($id);
-            },
-            'update_user' => function ($id) {
-                return $this->app->make(UserService::class)->getById($id);
-            },
-            'get_user' => function ($id) {
-                return $this->app->make(UserService::class)->getById($id);
-            },
-            'delete_user' => function ($id) {
-                return $this->app->make(UserService::class)->getById($id);
-            },
-        ];
     }
 }
