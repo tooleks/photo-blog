6aacb87aade2e2b94ea3195870566085d40c7a4a
tooleks <tooleks@gmail.com>
2017-02-15 14:02:08 +0200

    [BE]. Fixed variable names

--- backend/src/app/Console/Commands/CreateRoles.php
+++ backend/src/app/Console/Commands/CreateRoles.php
@@ -8,7 +8,6 @@ use Illuminate\Console\Command;
 /**
  * Class CreateRoles.
  *
- * @property Role roleModel
  * @package App\Console\Commands
  */
 class CreateRoles extends Command
@@ -28,26 +27,14 @@ class CreateRoles extends Command
     protected $description = 'Create roles';
 
     /**
-     * Create a new command instance.
-     *
-     * @param Role $roleModel
-     */
-    public function __construct(Role $roleModel)
-    {
-        parent::__construct();
-
-        $this->roleModel = $roleModel;
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        $this->roleModel->truncate();
-        $this->roleModel->insert([
+        Role::truncate();
+        Role::insert([
             ['name' => Role::NAME_ADMINISTRATOR],
             ['name' => Role::NAME_CUSTOMER],
         ]);
