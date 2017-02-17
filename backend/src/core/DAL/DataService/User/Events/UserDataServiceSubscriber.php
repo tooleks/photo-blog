eef1a995f7b93b0d89f58d9f38a97a99eea3737e
tooleks <tooleks@gmail.com>
2017-02-17 15:41:37 +0200

    [BE]. Fixed password issue

--- backend/src/core/DAL/DataService/User/Events/UserDataServiceSubscriber.php
+++ backend/src/core/DAL/DataService/User/Events/UserDataServiceSubscriber.php
@@ -4,6 +4,7 @@ namespace Core\DAL\DataService\User\Events;
 
 use Core\DAL\Models\User;
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Hashing\Hasher;
 use Illuminate\Support\Facades\Validator as ValidatorFactory;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
@@ -11,10 +12,21 @@ use Illuminate\Validation\ValidationException;
 /**
  * Class UserDataServiceSubscriber.
  *
+ * @property Hasher hasher
  * @package Core\DAL\DataService\User\Events
  */
 class UserDataServiceSubscriber
 {
+    /**
+     * UserDataServiceSubscriber constructor.
+     *
+     * @param Hasher $hasher
+     */
+    public function __construct(Hasher $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     /**
      * Register the listeners for the subscriber.
      *
@@ -22,7 +34,7 @@ class UserDataServiceSubscriber
      */
     public function subscribe($events)
     {
-        $events->listen('events.photoDataService.beforeSave', static::class . '@onBeforeSave');
+        $events->listen('events.userDataService.beforeSave', static::class . '@onBeforeSave');
     }
 
     /**
