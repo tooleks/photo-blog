129b387d517fab62ec9cf1bed3fc8598cd590522
tooleks <tooleks@gmail.com>
2017-02-26 11:16:10 +0200

    [FE]. Fixed scrolling through sidebar issue

--- frontend/src/app/components/app/app.component.ts
+++ frontend/src/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, ViewChild} from '@angular/core';
 import {EnvService, TitleService, AuthProviderService} from '../../../shared/services';
 
 import '../../../../public/app/css/styles.css';
@@ -9,6 +9,9 @@ import '../../../../public/app/css/styles.css';
     styles: [require('./app.component.css').toString()],
 })
 export class AppComponent {
+    @ViewChild('sideBarComponent') sideBarComponent:any;
+    private pageOverflow = '';
+
     constructor(@Inject(EnvService) private env:EnvService,
                 @Inject(TitleService) private title:TitleService,
                 @Inject(AuthProviderService) private authProvider:AuthProviderService) {
@@ -17,6 +20,31 @@ export class AppComponent {
     ngOnInit() {
         this.title.setTitle();
     }
+
+    private showSideBar = () => {
+        this.sideBarComponent.show();
+        this.freezePageScroll();
+    };
+
+    private hideSideBar = () => {
+        this.sideBarComponent.hide();
+        this.unfreezePageScroll();
+    };
+    
+    private toggleSideBar = () => {
+        this.sideBarComponent.isVisible() ? this.hideSideBar() : this.showSideBar();
+    };
+
+    private freezePageScroll = () => {
+        let top = window.scrollY;
+        this.pageOverflow = 'hidden';
+        window.onscroll = () => window.scroll(0, top);
+    };
+
+    private unfreezePageScroll = () => {
+        this.pageOverflow = '';
+        window.onscroll = null;
+    };
 
     private getCurrentYear = () => {
         return (new Date).getFullYear();
