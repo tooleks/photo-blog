54fb0e2972267b6ac6e8ae6673a742edcb4957d0
tooleks <tooleks@gmail.com>
2017-03-01 13:13:23 +0200

    [FE]. Improved page not found component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpModule, JsonpModule} from '@angular/http';
-import {AppComponent, SideBarComponent, TopBarComponent, PageNotFoundComponent, SignInFormComponent, SignOutComponent} from './components';
+import {
+    AppComponent,
+    SideBarComponent,
+    TopBarComponent,
+    PageNotFoundComponent,
+    SignInFormComponent,
+    SignOutComponent
+} from './components';
 import {AppRouting, AppRoutingProviders} from './app.routing';
 import {SharedModule} from '../shared/shared.module';
 import {PhotosModule} from '../photos/photos.module';
--- frontend/src/app/components/page-not-found/page-not-found.component.ts
+++ frontend/src/app/components/page-not-found/page-not-found.component.ts
@@ -1,9 +1,26 @@
 import {Component, Inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {NavigatorServiceProvider, NavigatorService} from '../../../shared/services';
 
 @Component({
     selector: 'page-not-found',
     template: require('./page-not-found.component.html'),
 })
 export class PageNotFoundComponent {
-    
+    private navigator:NavigatorService;
+
+    constructor(@Inject(Router) private router:Router,
+                @Inject(NavigatorServiceProvider) navigatorProvider:NavigatorServiceProvider) {
+        this.navigator = navigatorProvider.getInstance();
+    }
+
+    ngOnInit() {
+        this.navigateTo404Page();
+    }
+
+    private navigateTo404Page = () => {
+        if (this.router.url !== '/404') {
+            this.navigator.navigate(['/404']);
+        }
+    };
 }
