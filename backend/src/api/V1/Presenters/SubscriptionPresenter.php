a5b51ed23b8ab1eda628fc04f54e5eda5cb266a2
tooleks <tooleks@gmail.com>
2017-03-20 19:02:03 +0200

    [BE]. Fixed comments

--- backend/src/api/V1/Presenters/SubscriptionPresenter.php
+++ backend/src/api/V1/Presenters/SubscriptionPresenter.php
@@ -7,7 +7,8 @@ use Tooleks\Laravel\Presenter\Presenter;
 /**
  * Class SubscriptionPresenter.
  *
- * @property string text
+ * @property string email
+ * @property string token
  * @package Api\V1\Presenters
  */
 class SubscriptionPresenter extends Presenter
