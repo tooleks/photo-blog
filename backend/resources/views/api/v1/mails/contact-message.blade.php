44f7c2b912159bbb3c5587bd5093180ba4e835e5
tooleks <tooleks@gmail.com>
2017-03-22 21:58:00 +0200

    [BE]. Added subscription API endpoint

--- backend/resources/views/api/v1/mails/contact-message.blade.php
+++ backend/resources/views/api/v1/mails/contact-message.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <title></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-</head>
-<body style="margin: 0; padding: 0;">
-<p>{{ $name }} <a href="mailto:{{ $email }}">{{ $email }}</a> from {{ $client_ip_address ?? 'N/A' }}.</p>
-<p>{{ $subject }}</p>
-<p>{{ $text }}</p>
-</body>
-</html>
+@extends('api.v1.mails.layout.main')
+
+@section('content')
+    <p>{{ $name }} <a href="mailto:{{ $email }}">{{ $email }}</a> from {{ $client_ip_address ?? 'N/A' }}.</p>
+    <p>{{ $subject }}</p>
+    <p>{{ $text }}</p>
+@endsection
