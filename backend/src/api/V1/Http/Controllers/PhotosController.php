c0642380d89e437719e6b587d2605fefda3976fb
tooleks <tooleks@gmail.com>
2017-03-25 12:17:58 +0200

    [BE]. Updated comments formatting

--- backend/src/api/V1/Http/Controllers/PhotosController.php
+++ backend/src/api/V1/Http/Controllers/PhotosController.php
@@ -184,7 +184,6 @@ class PhotosController extends ResourceController
      *         }
      *     ]
      * }
-     *
      */
 
     /**
--- backend/src/api/V1/Http/Controllers/TagsController.php
+++ backend/src/api/V1/Http/Controllers/TagsController.php
@@ -61,7 +61,7 @@ class TagsController extends ResourceController
      *             "text": "nature"
      *         }
      *     ]
-     *  }
+     * }
      */
 
     /**
