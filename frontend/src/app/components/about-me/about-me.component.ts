8a0020dfaf05d0cd82410d6d2f5e2f5d5f9bfbe6
tooleks <tooleks@gmail.com>
2017-04-07 18:30:15 +0300

    [FE]. Added meta tags to about-me.component.ts

--- frontend/src/app/components/about-me/about-me.component.ts
+++ frontend/src/app/components/about-me/about-me.component.ts
@@ -1,17 +1,26 @@
 import {Component, OnInit} from '@angular/core';
-import {TitleService, EnvService} from '../../../shared/services';
+import {TitleService, MetaTagsService, EnvService} from '../../../shared/services';
 
 @Component({
     selector: 'about-me',
     templateUrl: 'about-me.component.html',
 })
 export class AboutMeComponent implements OnInit {
-    constructor(private title:TitleService, private env:EnvService) {
+    constructor(private title:TitleService, private metaTags:MetaTagsService, private env:EnvService) {
     }
 
     ngOnInit():void {
-        this.title.setTitle('About Me');
+        this.initTitle();
+        this.initMeta();
     }
+
+    private initTitle = ():void => {
+        this.title.setTitle('About Me');
+    };
+
+    private initMeta = ():void => {
+        this.metaTags.setTitle(this.title.getPageName());
+    };
 
     getContent = ():string => {
         return this.env.get('pageAboutMe');
