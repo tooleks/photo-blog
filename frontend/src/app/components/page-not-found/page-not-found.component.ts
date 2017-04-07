e8db7ce8a19d6142347b483ee3829869093ff5c8
tooleks <tooleks@gmail.com>
2017-04-07 19:08:23 +0300

    [FE]. Added meta tags to page-not-found.component.ts

--- frontend/src/app/components/page-not-found/page-not-found.component.ts
+++ frontend/src/app/components/page-not-found/page-not-found.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {NavigatorServiceProvider, NavigatorService} from '../../../shared/services';
+import {
+    TitleService,
+    MetaTagsService,
+    NavigatorServiceProvider,
+    NavigatorService
+} from '../../../shared/services';
 
 @Component({
     selector: 'page-not-found',
@@ -9,13 +14,26 @@ import {NavigatorServiceProvider, NavigatorService} from '../../../shared/servic
 export class PageNotFoundComponent implements OnInit {
     private navigator:NavigatorService;
 
-    constructor(private router:Router, navigatorProvider:NavigatorServiceProvider) {
+    constructor(private title:TitleService,
+                private metaTags:MetaTagsService,
+                private router:Router,
+                navigatorProvider:NavigatorServiceProvider) {
         this.navigator = navigatorProvider.getInstance();
     }
 
     ngOnInit():void {
+        this.initTitle();
+        this.initMeta();
         this.navigateTo404Page();
     }
+
+    private initTitle = ():void => {
+        this.title.setTitle('Page not found');
+    };
+
+    private initMeta = ():void => {
+        this.metaTags.setTitle(this.title.getPageName());
+    };
 
     private navigateTo404Page = () => {
         if (this.router.url !== '/404') {
