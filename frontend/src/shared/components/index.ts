c41cc0b73b80c6e656cc8c9c273e3f0ddad60725
tooleks <tooleks@gmail.com>
2017-04-11 19:34:47 +0300

    [FE]. Cleaning up

--- frontend/src/shared/components/index.ts
+++ frontend/src/shared/components/index.ts
@@ -1,2 +1,2 @@
-export * from './file-select-input/file-select-input.component';
-export * from './tags-select-input/tags-select-input.component';
+export * from './file-select-input';
+export * from './tags-select-input';
