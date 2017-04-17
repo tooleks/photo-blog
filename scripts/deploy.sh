875b4ac24e73a8aaac0d80982a7b389322b2f1c6
tooleks <tooleks@gmail.com>
2017-04-17 19:44:17 +0300

    [BE]. Added deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,47 @@
+root_path="$PWD/.."
+
+isProdMode() {
+    if [ "$1" == "prod" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+echo \Updating Sources
+cd "$root_path"
+echo "$PWD"
+git pull
+
+echo \Building Frontend Application
+cd "$root_path/frontend"
+echo "$PWD"
+if isProdMode $1; then
+    npm run build:prod
+else
+    npm run build
+fi
+
+echo \Migrating Database
+cd "$root_path/backend"
+echo "$PWD"
+php artisan migrate
+
+echo \Publishing Frontend Application
+cd "$root_path/frontend"
+echo "$PWD"
+rm -r public
+cp -r dist public
+
+echo \Generating REST API Documentation
+cd "$root_path/backend"
+echo "$PWD"
+php artisan generate:rest_api_documentation
+
+if isProdMode $1; then
+    echo \Restarting Web Servers
+    cd "$root_path"
+    echo "$PWD"
+    pm2 restart public/server.js
+    systemctl restart nginx php7.0-fpm
+fi
