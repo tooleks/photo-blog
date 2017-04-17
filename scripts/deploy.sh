8b84821118cd4badf22cfe2c7b449d93663a2623
tooleks <tooleks@gmail.com>
2017-04-17 20:18:08 +0300

    [BE]. Fixed deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,7 @@ echo \> Updating Sources
 cd "$root_path"
 echo "$PWD"
 git pull
+echo \> Done
 
 echo \> Building Frontend Application
 cd "$root_path/frontend"
@@ -21,31 +22,39 @@ if isProdMode $1; then
 else
     npm run build
 fi
+echo \> Done
 
 echo \> Migrating Database
 cd "$root_path/backend"
 echo "$PWD"
 php artisan migrate --force
+echo \> Done
 
 echo \> Publishing Frontend Application
 cd "$root_path/frontend"
 echo "$PWD"
 rm -r public
 cp -r dist public
+echo \> Done
 
 echo \> Generating REST API Documentation
 cd "$root_path/backend"
 echo "$PWD"
 php artisan generate:rest_api_documentation
+echo \> Done
 
 if isProdMode $1; then
     echo \> Restarting Frontend Application
     cd "$root_path/frontend/public"
     echo "$PWD"
     pm2 restart server.js
+    echo \> Done
+fi
 
+if isProdMode $1; then
     echo \> Restarting Backend Application
     cd "$root_path/frontend/public"
     echo "$PWD"
     sudo systemctl restart nginx php7.0-fpm
+    echo \> Done
 fi
