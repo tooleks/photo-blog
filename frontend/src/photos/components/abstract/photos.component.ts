55990616908f51c1f34a8c90d07b84a24b53c2a1
tooleks <tooleks@gmail.com>
2017-05-04 03:36:56 +0300

    [FE]. Refactored photos component

--- frontend/src/photos/components/abstract/photos.component.ts
+++ frontend/src/photos/components/abstract/photos.component.ts
@@ -28,10 +28,15 @@ export abstract class PhotosComponent implements OnInit, AfterViewInit {
     protected hasMoreImages:boolean = true;
     protected linkedData:Array<any> = [];
 
-    constructor(protected router:Router, protected route:ActivatedRoute,
-                protected app:AppService, protected title:TitleService, protected metaTags:MetaTagsService,
-                protected navigatorProvider:NavigatorServiceProvider, protected pagerProvider:PagerServiceProvider,
-                protected processLockerProvider:ProcessLockerServiceProvider, protected scrollFreezer:ScrollFreezerService) {
+    constructor(protected router:Router,
+                protected route:ActivatedRoute,
+                protected app:AppService,
+                protected title:TitleService,
+                protected metaTags:MetaTagsService,
+                protected navigatorProvider:NavigatorServiceProvider,
+                protected pagerProvider:PagerServiceProvider,
+                protected processLockerProvider:ProcessLockerServiceProvider,
+                protected scrollFreezer:ScrollFreezerService) {
         this.pager = this.pagerProvider.getInstance(this.defaults.page, this.defaults.perPage);
         this.navigator = this.navigatorProvider.getInstance();
         this.processLocker = this.processLockerProvider.getInstance();
@@ -56,40 +61,7 @@ export abstract class PhotosComponent implements OnInit, AfterViewInit {
             .subscribe((show:any) => this.queryParams['show'] = show ? Number(show) : this.defaults.show);
 
         this.originalImagesChange
-            .subscribe((originalImages:Array<any>) => {
-                this.linkedData = originalImages.map((image:any) => {
-                    return {
-                        '@context': 'http://schema.org',
-                        '@type': 'Article',
-                        'mainEntityOfPage': {
-                            '@type': 'WebPage',
-                            '@id': `${this.app.getUrl()}/photos?show=${image.id}`,
-                        },
-                        'headline': image.description,
-                        'image': {
-                            '@type': 'ImageObject',
-                            'url': image.thumbnails.large.url,
-                            'height': image.thumbnails.large.height,
-                            'width': image.thumbnails.large.width,
-                        },
-                        'datePublished': image.created_at,
-                        'dateModified': image.updated_at,
-                        'author': {
-                            '@type': 'Person',
-                            'name': this.app.getAuthor(),
-                        },
-                        "publisher": {
-                            "@type": "Organization",
-                            "name": this.app.getName(),
-                            "logo": {
-                                "@type": "ImageObject",
-                                "url": this.app.getImage(),
-                            }
-                        },
-                        'description': image.description,
-                    };
-                });
-            });
+            .subscribe((originalImages:Array<any>) => this.onOriginalImagesChange(originalImages));
     }
 
     reset():void {
@@ -108,14 +80,14 @@ export abstract class PhotosComponent implements OnInit, AfterViewInit {
     }
 
     protected onLoadImagesSuccess(response:any):Array<GalleryImage> {
-        const images = PhotoToGalleryImageMapper.map(response.data);
+        const images:Array<GalleryImage> = PhotoToGalleryImageMapper.map(response.data);
         this.hasMoreImages = !(response.data.length < this.defaults.perPage);
         if (response.data.length) {
             this.pager.setPage(response.current_page);
             this.navigator.setQueryParam('page', this.pager.getPage());
-            this.images = this.images.concat(images);
             this.originalImages = this.originalImages.concat(response.data);
             this.originalImagesChange.emit(this.originalImages);
+            this.images = this.images.concat(images);
         }
         return images;
     }
@@ -128,8 +100,39 @@ export abstract class PhotosComponent implements OnInit, AfterViewInit {
         return this.processLocker.isLocked();
     }
 
-    getLinkedData():any {
-        return this.linkedData;
+    onOriginalImagesChange(originalImages:Array<any>):void {
+        this.linkedData = originalImages.map((image:any) => {
+            return {
+                '@context': 'http://schema.org',
+                '@type': 'Article',
+                'mainEntityOfPage': {
+                    '@type': 'WebPage',
+                    '@id': `${this.app.getUrl()}/photos?show=${image.id}`,
+                },
+                'headline': image.description,
+                'image': {
+                    '@type': 'ImageObject',
+                    'url': image.thumbnails.large.url,
+                    'height': image.thumbnails.large.height,
+                    'width': image.thumbnails.large.width,
+                },
+                'datePublished': image.created_at,
+                'dateModified': image.updated_at,
+                'author': {
+                    '@type': 'Person',
+                    'name': this.app.getAuthor(),
+                },
+                "publisher": {
+                    "@type": "Organization",
+                    "name": this.app.getName(),
+                    "logo": {
+                        "@type": "ImageObject",
+                        "url": this.app.getImage(),
+                    }
+                },
+                'description': image.description,
+            };
+        });
     }
 
     onShowPhoto(image:GalleryImage):void {
