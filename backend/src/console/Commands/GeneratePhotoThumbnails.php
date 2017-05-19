b01d0eb29a04bcd95ad6a6362613598423713dc4
tooleks <tooleks@gmail.com>
2017-05-19 14:11:07 +0300

    [BE]. Fixed photo data provider 'each' method missed argument issues

--- backend/src/console/Commands/GeneratePhotoThumbnails.php
+++ backend/src/console/Commands/GeneratePhotoThumbnails.php
@@ -59,7 +59,7 @@ class GeneratePhotoThumbnails extends Command
             $this->comment("Generating photo thumbnails (id:{$photo->id}) ...");
             $this->generatePhotoThumbnails($photo);
             $this->comment("Photo thumbnails was successfully generated (id:{$photo->id}).");
-        }, ['with' => ['thumbnails']]);
+        }, 100, ['with' => ['thumbnails']]);
     }
 
     /**
