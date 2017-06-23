d9686c7bd9732a4c760c892ca030871858166ed0
tooleks <tooleks@gmail.com>
2017-06-23 16:46:21 +0300

    [FE]. Fixed the notices module duplicates issue

--- frontend/src/lib/notices/models/notice.ts
+++ frontend/src/lib/notices/models/notice.ts
@@ -1,13 +1,19 @@
 export class Notice {
-    timestampCreated: number;
+    createdAt: number;
     type: string;
     title: string;
     text: string;
 
-    constructor(timestampCreated: number, type: string, title: string, text: string = '') {
-        this.timestampCreated = timestampCreated;
+    constructor(createdAt: number, type: string, title: string, text: string = '') {
+        this.createdAt = createdAt;
         this.type = type;
         this.title = title;
         this.text = text;
+    }
+
+    isEqual(notice: Notice) {
+        return this.type === notice.type &&
+            this.title === notice.title &&
+            this.text == notice.text;
     }
 }
--- frontend/src/lib/notices/services/notices.service.ts
+++ frontend/src/lib/notices/services/notices.service.ts
@@ -4,47 +4,61 @@ import {Notice} from '../models';
 @Injectable()
 export class NoticesService {
     protected notices: Array<Notice> = [];
-    public deleteTimeout: number = 4000;
+    public deleteTimeout: number = 5000;
     public deleteInterval: number = 42;
 
     constructor() {
         // #browser-specific
         if (typeof (window) !== 'undefined') {
-            setInterval(() => this.shiftAfterDeleteTimeout(), this.deleteInterval);
+            setInterval(() => this.deleteOutdated(), this.deleteInterval);
         }
     }
 
+    exists(newNotice: Notice) {
+        return this.notices.filter((notice: Notice) => notice.isEqual(newNotice)).length;
+    }
+
     success(title: string, text?: string): void {
-        this.notices.push(new Notice(Math.floor(Date.now()), 'success', title, text))
+        const newNotice = new Notice(Math.floor(Date.now()), 'success', title, text);
+        if (!this.exists(newNotice)) {
+            this.notices.push(newNotice);
+        }
     }
 
     warning(title: string, text?: string): void {
-        this.notices.push(new Notice(Math.floor(Date.now()), 'warning', title, text))
+        const newNotice = new Notice(Math.floor(Date.now()), 'warning', title, text);
+        if (!this.exists(newNotice)) {
+            this.notices.push(newNotice);
+        }
     }
 
     error(title: string, text?: string): void {
-        this.notices.push(new Notice(Math.floor(Date.now()), 'error', title, text))
+        const newNotice = new Notice(Math.floor(Date.now()), 'error', title, text);
+        if (!this.exists(newNotice)) {
+            this.notices.push(newNotice);
+        }
     }
 
     info(title: string, text?: string): void {
-        this.notices.push(new Notice(Math.floor(Date.now()), 'info', title, text))
+        const newNotice = new Notice(Math.floor(Date.now()), 'info', title, text);
+        if (!this.exists(newNotice)) {
+            this.notices.push(newNotice);
+        }
     }
 
     get(): Array<Notice> {
         return this.notices;
     }
 
-    deleteByIndex(index: number): void {
-        this.notices = this.notices.filter((element, elementIndex) => elementIndex !== index);
-    }
-
     deleteAll(): void {
         this.notices = [];
     }
 
-    shiftAfterDeleteTimeout(): void {
-        if (this.notices.length && this.notices[0].timestampCreated + this.deleteTimeout < Math.floor(Date.now())) {
-            this.notices.shift();
-        }
+    deleteByIndex(index: number): void {
+        this.notices = this.notices.filter((element, elementIndex) => elementIndex !== index);
+    }
+
+    deleteOutdated(): void {
+        this.notices = this.notices.filter((notice: Notice) => notice.createdAt + this.deleteTimeout > Math.floor(Date.now()));
     }
 }
