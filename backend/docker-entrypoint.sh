bd167fddfbb02e333903d2bd4c5789702cb2cf35
tooleks <tooleks@gmail.com>
2017-11-19 16:02:36 +0200

    Fixed backend frontend preset

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,16 +2,18 @@
 
 set -e
 
-if [ "$REBUILD_ON_STARTUP" == "1" ]; then
-    chown -R www-data:www-data ./storage/
-    chown -R www-data:www-data ./bootstrap/cache/
-    composer install --no-interaction
-    composer dump-autoload
-    php artisan key:generate
-    php artisan migrate --force
-    php artisan create:roles
-    php artisan cache:clear
-    php artisan generate:rest_api_documentation
-fi
+chown -R www-data:www-data ./storage/
+chown -R www-data:www-data ./bootstrap/cache/
+
+composer install --no-interaction
+composer dump-autoload
+
+npm install
+
+php artisan key:generate
+php artisan migrate --force
+php artisan create:roles
+php artisan cache:clear
+php artisan generate:rest_api_documentation
 
 exec "$@"
