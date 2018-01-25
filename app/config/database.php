7c13e596293602d8a3900ffa5983ee4c5f43ce82
tooleks <tooleks@gmail.com>
2018-01-25 10:05:24 +0200

    Enabled persistent Redis connection

--- app/config/database.php
+++ app/config/database.php
@@ -103,6 +103,7 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
+            'persistent' => true,
         ],
 
     ],
