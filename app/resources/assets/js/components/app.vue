c255d5e2a3f44611bfc48cc21210daa2c15192e9
tooleks <tooleks@gmail.com>
2018-02-25 20:48:22 +0200

    Added router transition

--- app/resources/assets/js/components/app.vue
+++ app/resources/assets/js/components/app.vue
@@ -2,7 +2,9 @@
     <div id="container">
         <app-header class="app-header"></app-header>
         <div class="app-router">
-            <router-view></router-view>
+            <transition name="fade">
+                <router-view></router-view>
+            </transition>
         </div>
         <notifications group="main"></notifications>
         <app-footer></app-footer>
@@ -10,6 +12,19 @@
 </template>
 
 <style scoped>
+    .fade-enter-active, .fade-leave-active {
+        transition-property: opacity;
+        transition-duration: .2s;
+    }
+
+    .fade-enter-active {
+        transition-delay: .2s;
+    }
+
+    .fade-enter, .fade-leave-active {
+        opacity: 0
+    }
+
     .app-router {
         padding-top: 0;
         padding-bottom: 50px;
