fa8d9ac16ce34542ffaa59b2f7aa71e0d0a7223f
tooleks <tooleks@gmail.com>
2018-05-07 01:35:28 +0300

    Improved renderer configuration

--- renderer/server.js
+++ renderer/server.js
@@ -6,4 +6,8 @@ const server = prerender({
     chromeFlags: ["--no-sandbox", "--headless", "--disable-gpu", "--remote-debugging-port=9222", "--hide-scrollbars"],
 });
 
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+
 server.start();
