97df011295ecfc2bbb447dd17a0cc26c2cbdf6bd
tooleks <tooleks@gmail.com>
2018-09-15 13:34:15 +0300

    Updated directories structure

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -3,10 +3,10 @@
 const mix = require("laravel-mix");
 
 mix
-    .js("./resources/assets/js/app.js", "./public/js")
-    .sass("./resources/assets/sass/app.scss", "./public/css")
-    .copy("./resources/assets/images/**", "./public/images")
-    .copy("./resources/assets/images/favicons/**", "./public")
+    .js("./resources/js/app.js", "./public/js")
+    .sass("./resources/sass/app.scss", "./public/css")
+    .copy("./resources/images/**", "./public/images")
+    .copy("./resources/images/favicons/**", "./public")
     .options({
         extractVueStyles: true,
     });
