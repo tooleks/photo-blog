2d0aa2d40beb6472259e6da8bdc4e4f3e9dde41c
tooleks <tooleks@gmail.com>
2018-11-03 17:29:23 +0200

    Implemented decode search params function

---
+++ app/resources/js/utils/decodeSearchParams.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} search
+ * @return {Object}
+ */
+export default function decodeSearchParams(search) {
+    search = decodeURIComponent(search);
+
+    if (search.startsWith("?")) {
+        search = search.slice(1);
+    }
+
+    return search
+        .split("&")
+        .filter((item) => item.length > 0)
+        .reduce((params, item) => {
+            const [name, value] = item.split("=");
+            params[name] = value;
+            return params;
+        }, {});
+}
